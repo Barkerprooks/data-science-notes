fb2133da80fd5063f0f617a119af6db2c5822946
Parker Brooks <jonparkerbrooks@gmail.com>
2024-04-05 18:57:09 -0500

    added sets notes

--- hw.py
+++
@@ -1,9 +0,0 @@
-numbers = []
-
-for i in range(3):
-    user_input = input('input number: ')
-    number = float(user_input)
-    numbers.append(number)
-
-for number in numbers:
-    print(number * 2)
