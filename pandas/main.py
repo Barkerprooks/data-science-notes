ed5afc458b22780eedd5855e8b1fbef1d92c5561
Parker Brooks <jonparkerbrooks@gmail.com>
2024-04-26 19:00:34 -0500

    pandas progress

--- pandas/main.py
+++ pandas/main.py
@@ -1,24 +1,36 @@
-import random
 import pandas as pd
-import matplotlib.pyplot as plt
 
-delays = pd.read_csv('pandas/airline.csv')
-coordinates = pd.read_csv("pandas/coordinates.csv")
+# series = pd.Series([1, 2, 3, 4, 5])
+# new_series = series.apply(lambda x: x * 2)
 
-print('part 1: loaded CSV file, # of rows:', len(delays.index))
+# names = {'name': ['Jon Doe', 'Jane Doe', 'Mickey Mouse'],
+#               'age' : [22, 25, 24],
+#               'occ' : ['software engineer', 'Data Scientist', 'Clown']}
 
-f1 = delays['airport_name'].str.contains('TX')
-f2 = delays['airport_name'].str.contains('TN')
+# salary = {'money': [100, 200, 300],
+#          'name': ['Jon Doe', 'Jane Doe', 'Mickey Mouse']}
 
-print('part 3: list of Texas and Tennessee:', delays[f1 | f2])
+# df1 = pd.DataFrame(names)
+# df2 = pd.DataFrame(salary)
 
-flights = delays + coordinates # merge the datasets
+# merged = pd.merge(df1, df2, how='inner', on='name')
 
-fig, ax = plt.subplots()
-x = coordinates['long']
-y = coordinates['lat']
+# print(merged.describe())
 
-print(x)
 
-ax.scatter(x, y)
-plt.show()
+# 1. Read airline.csv into the program as a Pandas DataFrame
+# path: "pandas/airline.csv"
+table = pd.read_csv("pandas/airline.csv")
+
+# 2. Display all Texas and Tennessee airports
+texas_rows = table.airport_name.str.contains('TX')
+tennessee_rows = table.airport_name.str.contains('TN')
+print(table.loc[tennessee_rows | texas_rows]['airport_name'])
+
+# 3. Display how many arrivals into JFK in 2019 encountered both weather and carrier delays.
+jfk = table.airport == "JFK"
+year = table.year == 2019
+weather = table.weather_delay > 0
+carrier = table.carrier_delay > 0
+
+print(table.loc[jfk & year & weather & carrier])
